c76c284de716db7ad333802a5a96490528c2e9a7
Marília Sales <devmariliasales@gmail.com>
2023-11-11 15:25:40 -0300

    Conectando a API ao banco de dados

---
+++ src/controllers/cliente.controller.js
@@ -0,0 +1,32 @@
+const mongodb= require('mongodb').MongoClient
+
+
+const obj=[
+    {nome:"Ana",sobrenome:"Santos"},
+    {nome:"Bruna",sobrenome:"Santos"},
+    {nome:"Carla",sobrenome:"Sales"},
+    {nome:"Joana",sobrenome:"Nascimento"},
+    {nome:"Pedro",sobrenome:"Silva"},
+    {nome:"Matheus",sobrenome:"Santos"},
+]
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+
+ mongodb.connect(url,(erro,banco)=>{
+        if(erro)throw erro
+        const dbo=banco.db("Cadastro de nomes")
+        const colecao="cadastro"
+        dbo.collection(colecao).insertMany(obj,(erro,resultado)=>{
+            if(erro)throw erro
+             console.log(resultado.insertedCount + "novo curso inserido")
+            banco.close()
+        })  
+    })
+    await client.close();
+  }
+}
+run().catch(console.dir);
