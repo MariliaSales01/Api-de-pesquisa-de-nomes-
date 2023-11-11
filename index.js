fe235fcc46c97ecd1922d5e3d701833f403e9607
Marília Sales <devmariliasales@gmail.com>
2023-11-10 22:01:07 -0300

    Modularizando os arquivos

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const mongodb=require('mongodb').MongoClient
-const url = 'mongodb+srv://marilia:YoCccdWJGx5qCNr5@cluster0.izdwia8.mongodb.net/?retryWrites=true&w=majority'
+const mongodb= require('mongodb').MongoClient
+const url = "mongodb+srv://marilia:YoCccdWJGx5qCNr5@cluster0.izdwia8.mongodb.net/?retryWrites=true&w=majority";
 
 const obj=[
     {nome:"Ana",sobrenome:"Santos"},
@@ -9,14 +9,24 @@ const obj=[
     {nome:"Pedro",sobrenome:"Silva"},
     {nome:"Matheus",sobrenome:"Santos"},
 ]
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
 
-mongodb.connect(url,(erro,banco)=>{
-    if(erro)throw erro
-    const dbo=banco.db("Cadastro de nomes")
-    const colecao="cadastro"
-    dbo.collection(colecao).insertMany(obj,(erro,resultado)=>{
+ mongodb.connect(url,(erro,banco)=>{
         if(erro)throw erro
-         console.log(resultado.insertedCount + "novo curso inserido")
-        banco.close()
-    })  
-})
+        const dbo=banco.db("Cadastro de nomes")
+        const colecao="cadastro"
+        dbo.collection(colecao).insertMany(obj,(erro,resultado)=>{
+            if(erro)throw erro
+             console.log(resultado.insertedCount + "novo curso inserido")
+            banco.close()
+        })  
+    })
+    await client.close();
+  }
+}
+run().catch(console.dir);
